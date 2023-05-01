968cc2debf96869fb054d8883140df675ac56d3c
code123T <104305430+code123T@users.noreply.github.com>
2023-05-02 00:10:26 +0400

    Add files via upload

---
+++ Areas/Admin/Controllers/DashBoardController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace WebShopping.Areas.Admin.Controllers
+{
+    [Authorize(Roles ="")]
+    [Area("admin")]
+    [Route("admin/dashboard")]
+    public class DashBoardController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
